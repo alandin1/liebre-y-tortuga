aaf451995dd4dc1394ad01316a84a9297ad806ca
alandin1 <alandin1@ucol.mx>
2020-10-12 23:20:22 -0500

    tortuga

--- main.js
+++ main.js
@@ -12,3 +12,27 @@ class Corredor {
     }
   }
   
+  class Tortuga extends Corredor{
+    reaction(){
+      let suerte = this.getSuerte()
+      let pasos = 0
+      console.group("Turno de la tortuga")
+      if(suerte <= 50){
+        console.log("Paso rápido")
+        pasos = 3
+      }else if(suerte <=70) {
+        console.log("Resbalón")
+        pasos = -6
+      }else {
+        console.log("Paso lento")
+        pasos = 1
+      }
+      console.log(`Y avanzó ${pasos} pasos`)
+      console.groupEnd()
+      return this.run(pasos)
+    }
+  }
+  
+  class Liebre extends Corredor{
+    reaction(){
+      
